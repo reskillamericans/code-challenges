84e12dc8763b90aa4ea1a92352e4afe974717662
Mike Koss <mike@mckoss.com>
2021-08-31 07:43:18 -0700

    Begin refactoring of Runner - in TypeScript

--- scripts/maze-challenge.js
+++ scripts/maze-challenge.js
@@ -52,7 +52,6 @@ function runChallenge(solutionArg) {
         }
         mode = mode === 'paused' ? 'run' : 'paused';
         pauseRunBtn.innerText = mode === 'run' ? "Pause" : "Run";
-        console.log(mode);
     });
 
     let stepBtn = document.getElementById('step-btn');
---
+++ src/challenge-runner.ts
@@ -0,0 +1,134 @@
+export { Runner };
+
+class Runner {
+    stage : HTMLElement;
+    simulator;
+    controls = {};
+    mode = 'paused';
+    steps = 0;
+    solutionFunction;
+    solutionIterator;
+
+    static const controlIDs = ['pause-run-btn', 'step-btn', 'restart-btn', 'speed', 'steps-txt'];
+    static const controlEvents = ['click', 'click', 'click', 'change', undefined];
+    static const handlers = ['onPauseRun', 'onStep', 'onRestart', 'onSpeed', undefined];
+    static controlsHTML = `
+        <div class="panel">
+        <button id="pause-run-btn">Run</button>
+        <button id="step-btn">Step</button>
+        <button id="restart-btn">Restart</button>
+        Steps: <span id="steps">${steps}</span>
+        </div>
+        <div class="panel">
+            Speed: <select id="speed">
+                <option value="1000">Slow</option>
+                <option value="300" selected>Medium</option>
+                <option value="100">Fast</option>
+                <option value="0">Hyper-speed</option>
+            </select>
+        </div>`;
+
+    constructor(stage, simulator) {
+        this.stage = stage;
+        this.simulator = simulator;
+        simulator.appendVisualization(stage);
+        this.appendControls();
+        this.animate();
+    }
+
+    makeRunner() {
+        return (solutionFunction) => {
+            this.attachSolution(solutionFunction);
+        }
+    }
+
+    appendControls() {
+        this.stage.insertAdjacentHTML('beforeEnd', Runner.controlsHTML);
+        for (let i in controlIDs) {
+            let id = controlIDs[i];
+            controls[id] = document.getElementById(id);
+            if (controlEvents[i] !== undefined) {
+                controls[id].addEventListener(controlEvents[i], () => {
+                    this[handlers[i]]();
+                });
+            }
+        }
+    }
+
+    onPauseRun() {
+        if (this.mode === 'step') {
+            return;
+        }
+
+        this.mode = this.mode === 'paused' ? 'run' : 'paused';
+        this.setPauseRunText();
+    }
+
+    onStep() {
+        this.mode = 'step';
+        this.setPauseRunText();
+    }
+
+    onRestart() {
+        this.solutionIterator = undefined;
+        this.mode = 'paused';
+        this.setPauseRunText();
+
+        this.simulator.reset();
+        this.steps = 0;
+        this.simulator.update();
+        this.update();
+    }
+
+    setPauseRunText() {
+        this.controls['pause-run-btn'].innerText = this.mode === 'run' ? "Pause" : "Run"
+    }
+
+    update() {
+        this.controls['steps-txt'].innerText = `${this.steps}${this.mode === 'complete' ? "!" : ""}`;
+    }
+
+    animate() {
+        function nextFrame() {
+            requestAnimationFrame(() => this.animate());
+        }
+
+        if (this.mode === 'run') {
+            setTimeout(nextFrame, this.delay);
+        } else {
+            nextFrame();
+        }
+
+        if (!(this.mode === 'run' || this.mode === 'step')) {
+            return;
+        }
+
+        if (this.solutionIterator === undefined) {
+            try {
+                this.solutionIterator = this.solutionFunction(...this.simulator.solutionArgs());
+            } catch (e) {
+                console.log(`Solution threw an exception: ${e}`);
+                this.mode = 'complete';
+            }
+            this.steps++;
+        } else {
+            try {
+                if (this.solutionIterator.next().done) {
+                    this.mode = 'complete';
+                }
+                steps++;
+            } catch (e) {
+                console.log(`Solution threw an exception: ${e}`);
+                mode = 'complete';
+            }
+        }
+
+        this.simulator.update();
+        this.update();
+
+        // Step is a 1-shot.
+        if (this.mode === 'step') {
+            this.mode = 'paused';
+        }
+    }
+}
